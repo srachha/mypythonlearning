6f8f5f92158b86f61da3d1169c7f00a6170faf54
Sathvik Rachha <sathvik.rachha@gmail.com>
2022-10-12 19:17:18 -0500

    Aruba switch configuration - CLI

---
+++ configureswitch.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from urllib3 import Timeout
+
+
+url = "http://10.254.77.1:80/rest/v3/"
+# command = "show ip"
+
+def switchlogin(host,username,password):
+    fullurl = url + "login-sessions"
+    data = {"userName":username,"password":password} 
+    r = requests.Session()
+    urlsession = r.post(fullurl, data=data, timeout=1)
+    if urlsession.status_code != "200":
+        print("The API session to {} is unsuccessfull. Session Response={}".format(host,urlsession.status_code))
+    else:
+        sessioncookie = urlsession["cookie"]
+        print(sessioncookie)
+        return sessioncookie
+
+def commandsinput(command):
+    c = {"cmd":command}
+    sessioncookie = switchlogin("10.254.77.1","ecyt4t7","WuWosJagWi17") 
+    r = requests.post(url+ 'cli', header={"cookie":sessioncookie}, data=json.dumps(c), timeout=1)
+    print(r.text)
+
+
+def configureswitch():
+    with open('pln9configuration.txt','r') as f:
+        linecommand = f.readlines()
+        commandsinput(linecommand)
+    print(commandsinput)
+
+
+configureswitch()
+             
+# switchlogin("10.254.77.1","ecyt4t7","WuWosJagWi17"
